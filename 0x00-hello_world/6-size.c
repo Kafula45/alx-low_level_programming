6301e1c40886edd54d1370808c4b3b32c57ada75
Kafula45 <musupilakafula79@gmail.com>
2022-10-16 12:51:25 -0700

    last

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print differentsizes
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of the char: %lu byte(s)\n". (unsigned long) sizeof(a));
+	printf("size of the int: %lu byte(s)\n". (unsigned long) sizeof(b));
+       printf("size of the long int: %lu byte(s)\n". (unsigned long) sizeof(c)); 	
+       printf("size of the long long int: %lu byte(s)\n". (unsigned long) sizeof(d));
+      printf("size of the float: %lu byte(s)\n". (unsigned long) sizeof(f));
+      return (0);
+}  
+
+	
