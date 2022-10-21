a090ca75f68bd043415d6a3dd5ce10b8a9026899
Kafula45 <musupilakafula79@gmail.com>
2022-10-21 06:03:10 -0700

    task7 cd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - program that prints the lowercase alphabet in revers.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+
+
