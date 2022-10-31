c92b06f59c6946577d33c1bf5dd0e5b1995a8a74
Kafula45 <musupilakafula79@gmail.com>
2022-10-30 22:37:13 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - radom number
- *
+ * main - prints if the number is positive, negative or 0
  * Return:0
  */
 int main(void)
@@ -13,10 +14,9 @@ int main(void)
 
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
+	else if (n == 0)
 		printf("%d is zero\n", n);
-
+	else
+		printf("%d is negative\n", n);
 	return (0);
 }
